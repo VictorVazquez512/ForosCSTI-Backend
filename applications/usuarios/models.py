8471cd2dbb30704dc23a325591fafc0a2380baac
[VictorVazquez512] <[victorhugo.vazquezrojas@gmail.com]>
2022-11-30 20:49:32 -0700

    sex

--- applications/usuarios/models.py
+++ applications/usuarios/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-from simple_history.models import HistoricalRecords
 
 
 class UserManager(BaseUserManager):
@@ -13,11 +12,11 @@ class UserManager(BaseUserManager):
         user.save(using=self.db)
         return user
 
-    def create_user(self, username, email, name, last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name,last_name, password, False, False, **extra_fields)
+    def create_user(self, username, email, password=None, **extra_fields):
+        return self._create_user(username, email,password, False, False, **extra_fields)
 
-    def create_superuser(self, username, email, name, last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name,last_name, password, True, True, **extra_fields)
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        return self._create_user(username, email, password, True, True, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
